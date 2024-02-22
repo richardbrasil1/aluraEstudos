03fbe16d607c8553fbf7f53dfff49e92125b3ed1
Richard Brasil <richardbrasil1@hotmail.com>
2024-02-21 22:25:42 -0300

    projeto alura midi
    funções e arrays

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -1,1 +1,16 @@
-document.querySelector('.tecla_pom');
+function tocaSom(idElementoAudio) {
+    let idElementoAudio
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+    listaDeTeclas[contador].onclick = tocaSom;       
+    
+    contador = contador + 1;
+
+    console.log(contador);
+} 
